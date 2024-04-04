fccb666492065f761d0c2c6a1e13e6155b422c3b
StrangeSas <strange.sas@yandex.ru>
2024-04-04 21:07:18 +0700

    Half of the hh.ru scrapper

--- code/main.py
+++ code/main.py
@@ -4,6 +4,8 @@ import fake_useragent
 import time
 import json
 
+#https://www.youtube.com/watch?v=0_xwepyuV1E 6:53
+
 def get_links(text):
     ua = fake_useragent.UserAgent()
     data = requests.get(
@@ -23,8 +25,8 @@ def get_links(text):
             )
             if data.status_code == 200:
                 soup = BeautifulSoup(data.content, "lxml")
-                for a in soup.find_all("a", attrs={"class": "vacancy_search_suitable_item"}).find("span", attrs={"class": "serp-item__title-link-wrapper"}).find("a", attrs={"class": "bloko-link"}):
-                    yield f"https://hh.ru{a.attrs['href'].split('?')[0]}"
+                for i in soup.find("div", attrs={"data-qa":"vacancy-serp__results"}).find_all("div", attrs={"class":"serp-item_link"}):
+                    yield f'{i.find("a", "bloko-link").attrs["href"].split("?")[0]}'
         except Exception as e:
             print(f"{e}")
         time.sleep(1)
@@ -33,5 +35,22 @@ def get_resume(link):
     pass
 
 if __name__ == "__main__":
-    for a in get_links("BI-аналитик"):
-        print(a)
+   for a in get_links("BI-аналитик"):
+       print(a)
+"""
+ua = fake_useragent.UserAgent()
+data = requests.get(
+    url="https://krasnoyarsk.hh.ru/search/vacancy?L_save_area=true&text={text}&excluded_text=&area=113&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&page={page}",
+    headers={"user-agent": ua.random}
+)
+soup = BeautifulSoup(data.content, 'lxml')
+print()
+
+for i in soup.find("div", attrs={"data-qa":"vacancy-serp__results"}).find_all("div", attrs={"class":"serp-item_link"}):
+    print(i.find("a", "bloko-link"))
+    
+    
+    
+    for i in soup.find("div", attrs={"data-qa":"vacancy-serp__results"}).find_all("div", attrs={"class":"serp-item_link"}):
+                        print(i.find("a", "bloko-link"))
+"""
