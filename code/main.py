681cbf3adac8a5642ad0e0ea557accbc1e1a9b7e
StrangeSas <strange.sas@yandex.ru>
2024-04-08 18:49:25 +0700

    Work for today

--- code/main.py
+++ code/main.py
@@ -6,10 +6,22 @@ import json
 
 #https://www.youtube.com/watch?v=0_xwepyuV1E 6:53
 
+"""
+По сути, мне сейчас узко направленный скрипт на hh.ru - следует обобщить,
+Либо сделать отдельные скрипты для разных сайтов и уже между ними переключаться
+Я думаю в данном случае будет лучше исполнить именно многопроцессорность запросов.
+
+Сделать скрипты для напила всех заявок с разных сайтов.
+Сделать скрипт, что будет запускать всех их и обрабатывать нужные данные
+
+Потом полученные json обрабатывать в csv формат.
+
+"""
+
 def get_links(text):
     ua = fake_useragent.UserAgent()
     data = requests.get(
-        url="https://krasnoyarsk.hh.ru/search/vacancy?L_save_area=true&text={text}&excluded_text=&area=113&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&page={page}",
+        url="https://krasnoyarsk.hh.ru/search/vacancy?L_save_area=true&text={text}&excluded_text=&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&page={page}",
         headers={"user-agent": ua.random}
     )
     soup = BeautifulSoup(data.content, 'lxml')
@@ -20,16 +32,16 @@ def get_links(text):
     for page in range(page_count):
         try:
             data = requests.get(
-                url="https://krasnoyarsk.hh.ru/search/resume?text={text}&area=54&isDefaultArea=true&ored_clusters=true&order_by=relevance&search_period=0&logic=normal&pos=full_text&exp_period=all_time&page={page}",
+                url="https://krasnoyarsk.hh.ru/search/vacancy?L_save_area=true&text={text}&excluded_text=&salary=&currency_code=RUR&experience=doesNotMatter&order_by=relevance&search_period=0&items_on_page=50&page={page}",
                 headers={"user-agent": ua.random}
             )
             if data.status_code == 200:
                 soup = BeautifulSoup(data.content, "lxml")
-                for i in soup.find("main", attrs={"class":"resume-serp-content"}).find_all("div", attrs={"data-qa":"resume-serp__resume"}):
-                    yield f'https://hh.ru/{i.find("a", "bloko-link").attrs["href"].split("?")[0]}'
+                for i in soup.find("div", attrs={"data-qa":"vacancy-serp__results"}).find_all("div", attrs={"class":"serp-item"}):
+                    yield f'{i.find("a", "bloko-link").attrs["href"].split("?")[0]}'
         except Exception as e:
             print(f"{e}")
-        time.sleep(0.2)
+        time.sleep(0.1)
     print(page_count)
 def get_resume(link):
     ua = fake_useragent.UserAgent()
@@ -64,6 +76,6 @@ if __name__ == "__main__":
     data = []
     for a in get_links("BI-аналитик"):
         data.append(get_resume(a))
-        time.sleep(0.2)
+        time.sleep(0.1)
         with open("data.json","w",encoding="utf-8") as f:
             json.dump(data,f,indent=4,ensure_ascii=False)
